370719098a046f33a5bab2a9b588d23315d74e52
Olli-Pekka Turtio <olli-pekka.turtio@helsinki.fi>
2022-08-08 00:07:44 +0300

    modyfying db.py

--- db.py
+++ db.py
@@ -2,7 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 from app import app
 
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+#FIXME replacing postgres for postgresql because SQLAlchemy doesnt support 'postgres' dialect anymore, 
+# and heroku doesnt allow to overdrive the DATABASE_URL value (a detach needs to be done. check:
+# https://stackoverflow.com/questions/35061914/how-to-change-database-url-for-a-heroku-application)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("postgres", "postgresql")
 db = SQLAlchemy(app)
 
 class Users(db.Model):
@@ -11,6 +14,6 @@ class Users(db.Model):
     password = db.Column(db.String(255), nullable=False)
     
     def __repr__(self):
-        return '<Users %r>' % self.username
+        return '<User %r>' % self.username
     
 db.create_all()
