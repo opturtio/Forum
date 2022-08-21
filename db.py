673ba50a94f578affa732a192cb3967aeca0f319
Olli-Pekka Turtio <olli-pekka.turtio@helsinki.fi>
2022-08-21 21:26:37 +0300

    heroku database fix in db.py

--- db.py
+++ db.py
@@ -5,5 +5,11 @@ from app import app
 #FIXME replacing postgres for postgresql because SQLAlchemy doesnt support 'postgres' dialect anymore, 
 # and heroku doesnt allow to overdrive the DATABASE_URL value (a detach needs to be done. check:
 # https://stackoverflow.com/questions/35061914/how-to-change-database-url-for-a-heroku-application)
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+
+#For heroku:
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("postgres", "postgresql")
+
+#For local:
+#app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+
 db = SQLAlchemy(app)
