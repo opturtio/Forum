9a7b4ae2ed9a58a8a0ed1f1dec7ac4b7e2ee2acf
Olli-Pekka Turtio <olli-pekka.turtio@helsinki.fi>
2022-08-22 13:41:50 +0300

    fix db.py

--- db.py
+++ db.py
@@ -7,9 +7,9 @@ from app import app
 # https://stackoverflow.com/questions/35061914/how-to-change-database-url-for-a-heroku-application)
 
 #For heroku:
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("postgres", "postgresql")
+#app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("postgres", "postgresql")
 
 #For local:
-#app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 
 db = SQLAlchemy(app)
