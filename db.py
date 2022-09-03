0a212620d1283b7a68eeec24129fd05112b6bb50
Olli-Pekka Turtio <olli-pekka.turtio@helsinki.fi>
2022-09-04 00:35:34 +0300

    database fix for heroku

--- db.py
+++ db.py
@@ -3,9 +3,9 @@ from os import getenv
 from app import app
 
 #For heroku:
-#app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("postgres", "postgresql")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("postgres", "postgresql")
 
 #For local:
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+#app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 
 db = SQLAlchemy(app)
